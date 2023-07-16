21c88abe0286e592cae7eddf6f092fd27be88d61
Charu Shukla <nesojam@gmail.com>
2023-07-16 21:07:26 +0530

    Updated app

--- app/main.py
+++ app/main.py
@@ -172,8 +172,8 @@ def main():
     initial_sidebar_state="expanded"
   )
   
-  # with open("../assets/style.css") as f:
-  #   st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+  with open("assets/style.css") as f:
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
   
   input_data = add_sidebar()
   # st.write(input_data)
