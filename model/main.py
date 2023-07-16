c045982e0ef9acef98fddf85df6d0d7dfc56a5f5
Charu Shukla <nesojam@gmail.com>
2023-07-16 21:02:32 +0530

    Updated app & model

--- model/main.py
+++ model/main.py
@@ -32,7 +32,7 @@ def create_model(data):
 
 
 def get_clean_data():
-  data = pd.read_csv("../data/data.csv")
+  data = pd.read_csv("data/data.csv")
   
   data = data.drop(['Unnamed: 32', 'id'], axis=1)
   
